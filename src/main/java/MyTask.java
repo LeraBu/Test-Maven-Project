ff87257823f699bbb3c9198abea9ef07daec9b65
Lera Burtseva <burtsevaleria@gmail.com>
2022-04-10 16:00:27 +0300

    Initial commit

---
+++ src/main/java/MyTask.java
@@ -0,0 +1,9 @@
+public class MyTask {
+    public static void main(String[] args) {
+        System.out.println(" days later.");
+    }
+
+    public static int howManyDays(){
+        return 5;
+    }
+}
