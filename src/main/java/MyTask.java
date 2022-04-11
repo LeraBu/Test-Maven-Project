8e0d2dc242ffd9907d5fbe6d2d21f662181b4668
Lera Burtseva <burtsevaleria@gmail.com>
2022-04-11 19:57:38 +0300

    Solution1

--- src/main/java/MyTask.java
+++ src/main/java/MyTask.java
@@ -1,9 +1,14 @@
+import org.joda.time.Interval;
+import org.joda.time.LocalDate;
+import org.joda.time.Period;
+
 public class MyTask {
     public static void main(String[] args) {
-        System.out.println(" days later.");
+        LocalDate date1 = new LocalDate(2022, 1, 10);
+        System.out.println(howManyDays(date1));
     }
 
-    public static int howManyDays(){
-        return 5;
+    public static Period howManyDays(LocalDate date){
+        return new Period(LocalDate.now(), date);
     }
 }
